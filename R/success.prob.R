25973ef8de86dac430a369186502b74c3814587a
Chitran1987 <ghosal.chitran@gmail.com>
2025-08-01 02:06:55 +0200

    Restructured the code success.prob and added error checks

--- R/success.prob.R
+++ R/success.prob.R
@@ -1,12 +1,23 @@
 #write a function which shows the probability of one single succesful event
 
 success.prob <- function(p, n, pl=T){
+  ###error checking
+  if(p > 1){
+    stop('The argument p should be less than or equal to 1')
+  }
+  if(n%%1 != 0){
+    stop('n has to be a positive integer')
+  }
+  if(n <= 0){
+    stop('n has to be a positive integer')
+  }
+  ###code
   X <- seq(0, n) #The no of trials as a vector from 0 to n
   Y <- 1 - (1 - p)^X
+  df <- data.frame(X,Y)
+  names(df) <- c('trials', 'probability of success')
   if (pl){
     plot(X, Y, xlab='no of trials', ylab = 'probability of success', ylim=c(-0.0,1))
   }
-  df <- data.frame(X,Y)
-  names(df) <- c('trials', 'probability of success')
   return(df)
 }
