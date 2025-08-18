c3548a8418b68008e766dce8ff2189232cfea2ff
Chitran1987 <ghosal.chitran@gmail.com>
2025-08-18 19:26:15 +0200

    Changed slightly for testing

--- R/movavg.reg.R
+++ R/movavg.reg.R
@@ -1,13 +1,16 @@
 ####write a moving average function wih a regularization penalty
 rm(list = ls())
 library(StatsChitran)
+library(rgenoud)
+library(microbenchmark)
+#library(parallel)
 ClearPlot()
 movavg.reg <- function(X, Y, bn, fn, lambda, ord.min = 1, ord.max = 40, pl = T, grid.search = T){
 
   ##The order function which returns the objective function (SSE + regularazation term)
   obj.fun <- function(ord){
     ord <- round(ord)
-    df <- movavg(X, Y, bn, fn, ord)
+    df <- StatsChitran::movavg(X, Y, bn, fn, ord)
     SSE <- sum((df$Y - Y)^2) #The sum squared error term
     MSE <- SSE/length(df$Y) #The Mean squared error term
     d2Y <- num_diff(df$X, df$Y, pl = F, order = 2)
@@ -18,16 +21,17 @@ movavg.reg <- function(X, Y, bn, fn, lambda, ord.min = 1, ord.max = 40, pl = T, 
     return(res)
   }
   if(!grid.search){
-    L <- constrOptim(theta = (ord.min+ord.max)/2, f = obj.fun, ui = matrix(data = c(1, -1), nrow = 2, byrow = T), grad = NULL, ci = matrix(data = c(ord.min, -ord.max), nrow = 2, byrow = T), method = 'SANN')
-    df <- movavg(X, Y, bn, fn, ord = round(L$par))
+    L <- genoud(fn = obj.fun, nvars = 1, Domains = cbind(ord.min, ord.max), data.type.int = T, max = F)
+    df <- StatsChitran::movavg(X, Y, bn, fn, ord = round(L$par))
     L_ret <- vector(mode = 'list', length = 3)
     L_ret[[1]] <- L
     L_ret[[2]] <- df
     L_ret[[3]] <- L$par
-    if(pl){
-      plot(X, Y)
-      lines(df$X, df$Y, col = 'red')
-    }
+    names(L_ret) <-
+      if(pl){
+        plot(X, Y)
+        lines(df$X, df$Y, col = 'red')
+      }
     return(L_ret)
   }else{
     ord.val <- seq(ord.min, ord.max) #sequence of values to be sequenced
@@ -55,22 +59,26 @@ movavg.reg <- function(X, Y, bn, fn, lambda, ord.min = 1, ord.max = 40, pl = T, 
 
 ######Testing the function
 ###create the noisy dataset -------- #################
-X <- seq(-10, 10, by=0.25)
+X <- seq(-10, 10, by=0.05)
 noise.data <- function(X){
   G1 <- gauss(X, amp = 3, mu = -3, sig = 2, probability = F)
   L1 <- gauss(X, amp = sqrt(3), mu=5, sig = 3, probability = F)
-  Y <- G1 - L1
+  Y1 <- G1 - L1
   set.seed(13)
-  noise <- runif(min = -1, max = 1, length(Y))
-  Y <- Y + noise
+  noise <- rnorm(n = length(X), mean = 0, sd = 0.5)
+  Y <- Y1 + noise
   plot(X, Y, col = rgb(0.25,0,0,0.25))
-  return(data.frame(X, Y))
+  return(data.frame(X, Y, Y1))
 }
 
 df <- noise.data(X)
+
+###Test the function  --------------- ###############
 dfm1 <- movavg(df$X, df$Y, bn = 3, fn = 3, ord = 1)
 dfm2 <- movavg(df$X, df$Y, bn = 3, fn = 3, ord = 10)
 plot(df$X, df$Y)
 lines(dfm1$X, dfm1$Y, col = 'red')
 lines(dfm2$X, dfm2$Y, col = 'green')
-L <- movavg.reg(X = df$X, Y = df$Y, bn = 3, fn = 3, ord.min = 1, ord.max = 100, lambda = 0.1, grid.search = T)
+#L <- movavg.reg(X = df$X, Y = df$Y, bn = 3, fn = 3, ord.min = 50, ord.max = 200, lambda = 0.1, grid.search = F)
+microbenchmark::microbenchmark(run = movavg.reg(X = df$X, Y = df$Y, bn = 3, fn = 3, ord.min = 50, ord.max = 200, lambda = 0.05, grid.search = T), times = 1)
+#lines(df$X, df$Y1)
